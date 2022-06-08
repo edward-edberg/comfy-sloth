85f1631963c53fbc7eab9dcf01cbec02c4267bd8
Edward Edberg <edward.edberg.halim@gmail.com>
2022-06-07 17:01:28 -0700

    add login and logout

--- src/index.js
+++ src/index.js
@@ -9,12 +9,21 @@ import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-btt17-e9.us.auth0.com"
+    clientId="mvVNeBvstxJcMV277CcTyZWpa4aJQwoG"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
